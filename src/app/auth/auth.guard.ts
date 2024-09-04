00699102e14e3921481b630f33801bf037d98b31
Suresh Manikandan A <sureshmanikandan@wimoku.com>
2024-09-04 22:02:20 +0530

    Added auth service and auth guard

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, RouterStateSnapshot } from "@angular/router";
+import { Observable, map } from "rxjs";
+import { AuthService } from "./auth.service";
+
+
+@Injectable({providedIn: 'root'})
+export class AuthGurad implements CanActivate{
+autheService = inject(AuthService);
+
+    canActivate(route: ActivatedRouteSnapshot, 
+        state: RouterStateSnapshot
+    ): boolean | Observable<boolean> {
+        
+        return this.autheService.user.pipe(map( user => {
+            return !!user;
+        }))
+       
+    }
+
+}
